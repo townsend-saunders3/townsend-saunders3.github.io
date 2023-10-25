2bfcee9a37576d91f974e56ce9a38242830cb7a4
Townsend <townsend.saunders3@gmail.com>
2023-10-25 13:13:49 -0700

    Added some styles and additional pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div style={{ display: "flex" }}>
+        <div className="sidebar">
+          <h2>My Portfolio</h2>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="content">
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my portfolio!</h1>
+      <button className="button">Click Me</button>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <button className="button">Learn More</button>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <button className="button">See More</button>
     </div>
   );
 }
