c2479ea438a7cdfd8d52bac62c9d3a51cfd54382
Townsend <townsend.saunders3@gmail.com>
2024-06-06 11:52:58 -0700

    Updated titles” npm run build npm run deploy

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -15,7 +15,7 @@ function Testimonials() {
         <div className="testimonials-grid">
           <div className="card">
             <img src={dom} alt="Dominique Parrish" className="testimonial-image" />
-            <div className="bio">American Freestyle Wrestler, 2022 World Champion</div>
+            <div className="bio">Dominique Parrish, American Freestyle Wrestler, 2022 World Champion</div>
             <div className="testimonial">"Townsends wide range of interests and technical skills gives him the ability to approach problems with a unique perspective. 
             His development of his wrestling app provided me insights about my wrestling. 
             I was then able use the information to develop practice plans."</div>
@@ -23,7 +23,7 @@ function Testimonials() {
 
           <div className="card">
             <img src={tanatswa} alt="Tanatswa Mapondera" className="testimonial-image"  />
-            <div className="bio">Investment Associate at Aligned Climate Capital</div>
+            <div className="bio">Tanatswa Mapondera, Investment Associate at Aligned Climate Capital</div>
             <div className="testimonial">
             “Townsend and I have been close friends since meeting in college. 
             I’ve always admired his voracious appetite for learning and improvement, evidenced by successes in the classroom, workplace, sports, and every other facet of his life. 
@@ -34,7 +34,7 @@ function Testimonials() {
           {/* Repeat the structure for the rest of the testimonials */}
           <div className="card">
             <img src={helen} alt="Helen Maroulis" className="testimonial-image"  />
-            <div className="bio">2016 Olympic Gold Medalist @ Team USA</div>
+            <div className="bio">Helen Maroulis, 2016 Olympic Gold Medalist @ Team USA</div>
             <div className="testimonial">Townsend is an excellent asset to any team. He is very brilliant in applying all that he’s learned to better the systems and world around him. His analytical skills, intelligence, and empathy have contributed to his growth as a human, teammate, and professional in the field</div>
           </div>
         </div>
