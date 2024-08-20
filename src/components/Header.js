81fc2f1090bf91db55c6d0eaa2273a99bf0c09fb
Townsend <townsend.saunders3@gmail.com>
2024-08-20 11:10:56 -0700

    fixed corrupt file

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
         </li>
         <li className="nav-item">
 
-            <a href="Townsend_Saunders_III_Resume_7.0.3" download="townsend_saunders_3_resume.pdf">Download Resume</a>
+            <a href="Townsend_Saunders_III_Resume_7.0.3.pdf" download="townsend_saunders_3_resume.pdf">Download Resume</a>
 
         </li>
       </ul>
